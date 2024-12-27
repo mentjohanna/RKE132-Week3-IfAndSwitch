687df62d72969a028f323511fade04313751e490
Johanna Kruusement <kruusement.johanna@gmail.com>
2024-12-27 13:31:13 +0200

    add week3 code

---
+++ IfDayOfTheWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek); // Sõltub mis päeval käima panna, siis võtab automaatselt päeva.
+
+if (weekDay == 0) // Päev
+{
+    Console.WriteLine("It's Sunday!"); 
+}
+else if (weekDay == 1)
+{
+    Console.WriteLine("It's Monday!");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("It's Tuesday!");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("It's Wednesday!");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("It's Thursday!");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("It's Friday!");
+}
+else
+{
+    Console.WriteLine("It's Saturday!"); // viimane valik 
+}
+
+Console.WriteLine("Have a nice day!"); // Lõpu soov
